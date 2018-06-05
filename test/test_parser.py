000d8de8aad4c6941ab9e77e3fbb392bb323e52a
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-06-04 23:08:25 -0400

    Naive addition of object_pairs_hook
    #6

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import collections
 import sys
 if sys.version_info < (2, 7):
     from StringIO import StringIO
@@ -22,3 +23,15 @@ def test_when_fileobj_has_no_name_attr_then_repr_of_fileobj_is_used_in_errors():
     source = StringIO("[")
     error = pytest.raises(toml.TomlError, lambda: toml.load(source))
     assert error.value.filename == repr(source)
+
+
+def test_object_pairs_hook():
+    source = StringIO(u"""\
+    [x.a]
+    [x.b]
+    [x.c]
+    """)
+
+    d = toml.load(source, object_pairs_hook=collections.OrderedDict)
+    assert list(d['x'].keys()) == sorted(d['x'].keys())
+    assert isinstance(d['x'], collections.OrderedDict)
